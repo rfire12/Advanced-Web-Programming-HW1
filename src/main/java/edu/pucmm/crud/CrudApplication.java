a7ddbe16b446161f513d37929eeddaf2759df82f
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-05-25 15:27:29 -0400

    Fixes Whitelabel Error Page

--- src/main/java/edu/pucmm/crud/CrudApplication.java
+++ src/main/java/edu/pucmm/crud/CrudApplication.java
@@ -1,11 +1,14 @@
 package edu.pucmm.crud;
 
+import edu.pucmm.crud.controllers.StudentsController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses= StudentsController.class)
 public class CrudApplication {
 
     public static void main(String[] args) {

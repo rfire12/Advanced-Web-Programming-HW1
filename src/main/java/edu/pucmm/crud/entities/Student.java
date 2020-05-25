377fe6ba7eb096c3a620b595b0ac72a5430e1e47
José Gabriel González <jgg032899@gmail.com>
2020-05-25 15:10:51 -0400

    CRUD API actions, finished

---
+++ src/main/java/edu/pucmm/crud/entities/Student.java
@@ -0,0 +1,49 @@
+package edu.pucmm.crud.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Student() {
+
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
